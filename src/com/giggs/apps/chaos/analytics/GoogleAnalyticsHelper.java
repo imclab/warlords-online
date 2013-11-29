e61bbf5940b1cb232dc2110d1025312e734fdcfc
guillaume <guillaume@ubuntu.(none)>
2013-11-29 16:50:16 +0000

    forgot analytics

--- src/com/giggs/apps/chaos/analytics/GoogleAnalyticsHelper.java
+++ src/com/giggs/apps/chaos/analytics/GoogleAnalyticsHelper.java
@@ -3,6 +3,7 @@ package com.giggs.apps.chaos.analytics;
 import android.content.Context;
 
 import com.google.analytics.tracking.android.EasyTracker;
+import com.google.analytics.tracking.android.MapBuilder;
 import com.google.analytics.tracking.android.Tracker;
 
 public class GoogleAnalyticsHelper {
@@ -17,8 +18,7 @@ public class GoogleAnalyticsHelper {
 
     public static void sendEvent(Context context, EventCategory category, EventAction action, String label) {
         EasyTracker easyTracker = EasyTracker.getInstance(context);
-        // easyTracker.send(MapBuilder.createEvent(category.name(),
-        // action.name(), label, null).build());
+        easyTracker.send(MapBuilder.createEvent(category.name(), action.name(), label, null).build());
     }
 
     public static enum TimingCategory {
@@ -31,8 +31,7 @@ public class GoogleAnalyticsHelper {
 
     public static void sendTiming(Context context, TimingCategory category, TimingName name, long loadTime) {
         Tracker easyTracker = EasyTracker.getInstance(context);
-        // easyTracker.send(MapBuilder.createTiming(category.name(), loadTime,
-        // name.name(), null).build());
+        easyTracker.send(MapBuilder.createTiming(category.name(), loadTime, name.name(), null).build());
     }
 
 }
